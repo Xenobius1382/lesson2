5d2df5258203683264b0a29d10f00776f8d71b08
Xenobius1382 <shubinmax13@gmail.com>
2024-06-21 01:02:26 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "переменные"
+
+Num_of_HW = 12
+Num_of_hrs = 1.5
+Name_of_crs = "Python"
+One_HW_time = Num_of_hrs / Num_of_HW
+print("Курс:" + Name_of_crs + ", " + "всего задач:", Num_of_HW,  ", " + "затрачено часов:", Num_of_hrs,",", "среднее время выполнения", One_HW_time, "часа.")
